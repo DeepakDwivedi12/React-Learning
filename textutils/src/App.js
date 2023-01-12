5e74f92cbd5b2682be2e70c4e124bab7c2bd1b18
DEEPAK DWIVEDI <deepakdwivedi4411@gmail.com>
2023-01-12 08:25:00 +0530

    First Commit -1

---
+++ textutils/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+let name="Deepak"
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <img src="" alt="" />
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate corrupti fuga obcaecati saepe explicabo ipsa tempora itaque eligendi, ea nihil quam rerum minima numquam nam ratione? Rem veniam aut voluptates, quia quod maxime itaque sunt?</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
