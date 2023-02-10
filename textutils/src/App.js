fc06c9143b6ee012c8f516910203518c71d4ad75
DEEPAK DWIVEDI <deepakdwivedi4411@gmail.com>
2023-02-10 07:28:10 +0530

    second commit

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,32 +1,58 @@
-import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
-  const [mode,setMode]= useState('light');
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
     }
-    else{
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
     }
   }
   return (
     <>
-<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-    <div className="container my-3">
-    <TextForm  heading="Enter your text analyze below"mode={mode}/>
-    {/* <About/> */}
-    </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path='/about' element={<About />} />
+            <Route exact path='/' element={<TextForm />} />
+          </Routes>
+          {/* <TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} /> */}
+
+        </div>
+      </Router>
     </>
   );
 }
 
 export default App;
-//http://192.168.1.48:3000
